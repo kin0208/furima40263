3a44a60a0a4512ef71ff48f72fe937bb9c9b60ee
kin0208 <kimsangsu110208@gmail.com>
2024-01-23 17:20:51 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,14 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :day
 
+  validates :name,          presence: true
+  validates :description,   presence: true
+  validates :category_id,   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,         presence: true
+  validates :image,         presence: true
+
 end
