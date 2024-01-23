21fc5b5ad05653488d294d88f12c75ed6e3205f4
kin0208 <kimsangsu110208@gmail.com>
2024-01-23 16:30:34 +0900

    モデルとアソシエーション

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :day
+
+end
