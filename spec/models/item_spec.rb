74e0834695aaef6b3d76760a3fd8966f239c973d
kin0208 <kimsangsu110208@gmail.com>
2024-01-25 18:08:44 +0900

    修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -37,8 +37,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
+      it '商品のカテゴリーの選択が---だと出品できない' do
+        category = Category.find_by(name: '---')
+        @item.category = category
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
       it '商品の状態が未選択だと出品できない' do
         @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it '商品の状態の選択が---だと出品できない' do
+        condition = Condition.find_by(name: '---')
+        @item.condition = condition
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
@@ -49,8 +63,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Fee can't be blank")
       end
 
+      it '配送料の負担の選択が---だと出品できない' do
+        fee = Fee.find_by(name: '---')
+        @item.fee = fee
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Fee can't be blank")
+      end
+
       it '発送元の地域が未選択だと出品できない' do
         @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it '発送元地域の選択が---だと出品できない' do
+        prefecture = Prefecture.find_by(name: '---')
+        @item.prefecture = prefecture
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
@@ -61,16 +89,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Day can't be blank")
       end
 
+      it '発送までの日数の選択が---だと出品できない' do
+        day = Day.find_by(name: '---')
+        @item.day = day
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
+
       it '価格が空だと出品できない' do
         @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
 
-      it '価格が¥300~¥9,999,999の間でなければ出品できない' do
+      it '価格が300円未満だと出品できない' do
         @item.price = 100
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it '価格が10000000円以上だと出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
 
       it '価格が半角数値でなければ出品できない' do
