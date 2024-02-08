0e12684669ba37756f80ef9a84bf3340c7edfce6
kin0208 <kimsangsu110208@gmail.com>
2024-02-08 12:34:00 +0900

    購入機能のルーティングとコントローラーを作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,21 @@
+class OrdersController < ApplicationController
+  def index
+    @order_delivery = OrderDelivery.new
+  end
+
+  def create
+    @order_delivery = OrderDelivery.new(order_params)
+    if @order_delivery.valid?
+      @order_delivery.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: current_item.id)
+  end
+end
