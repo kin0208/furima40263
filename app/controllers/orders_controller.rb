8ff60e13a66e5f304241d6a1086e15f4e683cea4
kin0208 <kimsangsu110208@gmail.com>
2024-02-08 18:40:49 +0900

    購入ページに遷移とバリデーションの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,11 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
       @order_delivery.save
@@ -16,6 +18,6 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: current_item.id)
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -3,11 +3,11 @@ class OrderDelivery
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
-    validates :prefecture_id
+    validates :postal_code,   format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
     validates :street_address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number" }
+    validates :phone_number,  format: { with: /\A\d{10,11}\z/, message: "is too short" }, numericality: { message: "is invalid. Input only numbers" }
   end
 
   def save
