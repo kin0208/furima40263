f94366fa1430678cb51f9295ad44083f89d0e7bc
kin0208 <kimsangsu110208@gmail.com>
2024-02-09 12:43:10 +0900

    クレジットカード決済の導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :redirect_to_top_if_not_selling_or_sold, only: [:index]
+
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
   end
@@ -8,9 +12,11 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new(order_params)
     if @order_delivery.valid?
+      pay_item
       @order_delivery.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -18,6 +24,26 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:order_delivery).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+     Payjp::Charge.create(
+       amount: @item.price,
+       card: order_params[:token],
+       currency: 'jpy'
+      )
+  end
+
+  def redirect_to_top_if_not_selling_or_sold
+    if user_signed_in?
+      item = Item.find(params[:item_id])
+      if current_user == item.user || item.sold_out?
+        redirect_to root_path
+      end
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,8 +1,9 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :postal_code,   format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :city
@@ -11,7 +12,7 @@ class OrderDelivery
   end
 
   def save
-    oreder = Order.create(user_id: user_id, item_id: item_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Delivery.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, street_address: street_address, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
