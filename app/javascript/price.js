bc8a6e25f54d555f0cdd159dcd3ddf34b6711084
kin0208 <kimsangsu110208@gmail.com>
2024-02-12 11:46:48 +0900

    renderで戻った時もJavaScriptできるように修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -13,3 +13,19 @@ window.addEventListener('turbo:load', () => {
     profitDom.innerHTML = ` ${profit}`;
   })
 });
+
+window.addEventListener('turbo:render', () => {
+  
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    const tax = Math.floor(inputValue * 0.1);
+    addTaxDom.innerHTML = ` ${tax}`;
+
+    const profitDom = document.getElementById("profit");
+    const profit = Math.floor(inputValue * 0.9);
+    profitDom.innerHTML = ` ${profit}`;
+  })
+});
